63ed5719cff610502c979f44645a9dac1fb5f627
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-11-30 22:22:56 -0500

    Added comment about given code

--- CppND-Capstone-Snake-Game-master/src/renderer.h
+++ CppND-Capstone-Snake-Game-master/src/renderer.h
@@ -12,7 +12,7 @@ class Renderer {
  public:
   Renderer(const std::size_t screen_width, const std::size_t screen_height,
            const std::size_t grid_width, const std::size_t grid_height);
-  ~Renderer();
+  ~Renderer();    // 1 : destructor
 
   void Render(Snake const snake, SDL_Point const &food, unsigned char snakeBodyColorHex[4], unsigned char snakeHeadColorHex[4], unsigned char foodHexColor[4]);  // used to render Snake object & food to screen
   void UpdateWindowTitle(int score, int fps); // updates bar at top of game window with score & frames per second
