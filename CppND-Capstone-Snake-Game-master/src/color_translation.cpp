7bd0c8867fe25776c26615bcac8a58c720cc7c2e
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-12-05 20:13:50 -0500

    Code for Object Oriented Programming: added 1 class

---
+++ CppND-Capstone-Snake-Game-master/src/color_translation.cpp
@@ -0,0 +1,11 @@
+#include "color_translation.h"
+
+color_translation::color_translation()
+{
+
+}
+
+color_translation::~color_translation()
+{
+
+}
---
+++ CppND-Capstone-Snake-Game-master/src/color_translation.h
@@ -0,0 +1,16 @@
+#ifndef COLOR_TRANSLATION_H
+#define COLOR_TRANSLATION_H
+
+#pragma once
+
+class color_translation
+{
+public:
+    color_translation();
+    ~color_translation();
+
+private:
+
+};
+
+#endif
